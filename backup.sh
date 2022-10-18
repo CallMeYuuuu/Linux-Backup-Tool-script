785f2e224dd89e54480ef748ec4975bcd4a1730d
CallMeYuu <44119696+CallMeYuuuu@users.noreply.github.com>
2022-10-18 11:59:33 +0200

    Init

---
+++ backup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+PASSED=$1
+
+YELLOW='\033[1;33m'
+LIGHT_GREEN='\033[1;32m'
+LIGHT_RED='\033[1;31m'
+LIGHT_BLUE='\033[1;34m'
+NC='\033[0m' # No Color
+
+if ! command -v zip &> /dev/null
+then
+    echo -e "${NC}[${LIGHT_BLUE}Backup Tool${NC}] ${LIGHT_RED}zip is not installed! ${YELLOW}Installing zip...${NC}"
+    apt install zip
+fi
+
+if [ -d "${PASSED}" ]
+	then
+		zip -r "$(date +"%Y.%m.%d-%H:%M")_$1-backup.zip" $1
+		echo -e "${NC}[${LIGHT_BLUE}Backup Tool${NC}] ${LIGHT_GREEN}Backup of directory ${YELLOW}$1 ${LIGHT_GREEN}was successful.${NC}"
+		if [ -d "BACKUP" ]
+			then
+				mv "$(date +"%Y.%m.%d-%H:%M")_$1-backup.zip" "BACKUP"
+			else
+				mkdir BACKUP
+				mv "$(date +"%Y.%m.%d-%H:%M")_$1-backup.zip" "BACKUP"
+		fi
+	else
+		echo -e "${NC}[${LIGHT_BLUE}Backup Tool${NC}] Usage: ./backup.sh <directory path>"
+		echo -e "${NC}[${LIGHT_BLUE}Backup Tool${NC}] ${LIGHT_RED}Please specify a directory path to back up!${NC}"
+fi
